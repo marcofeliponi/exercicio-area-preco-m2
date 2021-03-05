b19ddbd7fe24777bf647438d9dbd2b6368c52b6f
Marco Antonio <79797841+marcofeliponi@users.noreply.github.com>
2021-03-05 19:54:48 -0300

    first commit

---
+++ area-terreno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoM2, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoM2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = precoM2 * area;
+
+            Console.WriteLine("Largura " + "= " + largura.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Comprimento " + "= " + comprimento.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area" + "= " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor" + "= " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            
+
+
+        }
+    }  
+}
